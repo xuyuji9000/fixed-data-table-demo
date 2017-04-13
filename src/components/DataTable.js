8bf6f333f37944e9d33a4169cf1986d9f96552fb
Karl Xu <karl.yogi.xu@gmail.com>
2017-04-13 18:04:23 +0800

    add_demo

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -4,33 +4,18 @@ import { Table, Column, Cell } from 'fixed-data-table'
 
 const DataTable = createReactClass({
     render: function() {
-        const rows = [
-            ['a1', 'b1', 'c1'],
-            ['a2', 'b2', 'c2'],
-            ['a3', 'b3', 'c3']
-        ]
         return (
             <Table
+                rowsCount={50}
                 rowHeight={50}
-                rowsCount={rows.length}
-                width={5000}
-                height={5000}
-                headerHeight={50}
+                width={1000}
+                height={500}
             >
                 <Column
-                    header={<Cell>Col 1</Cell>}
-                    cell={<Cell>Column 1 static content</Cell>}
-                    width={2000}
-                ></Column>
-                <Column
-                    header={<Cell>Col 3</Cell>}
-                    cell={({rowIndex, ...props}) => (
-                        <Cell {...props}>
-                            Data for column 3: {rows[rowIndex][2]}
-                        </Cell>
-                    )}
-                    width={2000}
-                ></Column>
+                    cell={ <Cell>Basic Content</Cell> }
+                    width={200}
+                >
+                </Column>
             </Table>
         )
     }
